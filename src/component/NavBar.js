aabe033e4a865032b2e65a44b6a6260c332afbba
April W <april.weaver@revature.net>
2023-04-08 17:02:09 -0400

    fixing bars 1

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -53,7 +53,7 @@ export default function NavBar() {
                     <ul>
                         <li className="special">
                             <button className={classNames} onClick={viewNavBar} style={{backgroundColor: toggleNavBar===true? 'rgba(255, 255, 255, 0.2)':'inherit'}}>
-                                <img src="..\assets\images\bars.svg" alt="menu" className="bars"></img>
+                                <img src="\orison\src\assets\images\bars.svg" alt="menu" className="bars"></img>
                             </button>
                         </li>
                     </ul>
