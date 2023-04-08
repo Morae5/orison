95e173fc5d5c48afe4a3915b57422f93face5174
April W <april.weaver@revature.net>
2023-04-08 14:43:32 -0400

    fix css paths

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-// import Footer from './component/Footer'
-import Script from './component/Scripts'
+import Footer from './component/Footer';
+import Script from './component/Scripts';
 import Home from './pages/landing-page/Home';
 import NavBar from './component/NavBar';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
